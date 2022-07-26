12032ca369e5a57f5abfbecb99c9d02c03ca6716
Mohammed Jewel <jewelnabin@gmail.com>
2022-07-26 16:04:53 +0600

    Add a Add Review Section

---
+++ src/Pages/HomePage/FAQs/AddReview.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+const AddReview = () => {
+    return (
+      <div>
+        <div className="w-full py-12 px-48 bg-gray-200">
+          <div className="grid grid-cols-2 shadow-md rounded border-3">
+            <div className="bg-blue-500 rounded p-5 text-center text-white">
+              <h1 className="text-3xl font-bold">Fisheries Management</h1>
+              <p className="text-gray-300">
+                <small>
+                  Hello Admin, Login For Manage All Employee. If You Are An
+                  Admin We Give You An Login ID And Password. Enter Your Adimin
+                  ID And Password For Login.
+                </small>
+              </p>
+              <br />
+              <h2 className="text-gray-100">Are You Employee? Login here.</h2>
+              <h2 className="text-gray-100 underline">
+                <a href="#">Employee Login</a>
+              </h2>
+              <p className="text-gray-300">
+                <small>
+                  Read our <a href="#">terms</a> and <a href="#">conditions</a>
+                </small>
+              </p>
+            </div>
+            <div className="p-5">
+              <div>
+                <form class="">
+                  <div class="mb-4">
+                    <label
+                      class="block text-gray-500 text-sm font-bold mb-2"
+                      for="username"
+                    >
+                      Admin Username
+                    </label>
+                    <input
+                      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="username"
+                      type="text"
+                      placeholder="Enter Admin Username"
+                    />
+                  </div>
+                  <div class="mb-6">
+                    <label
+                      class="block text-gray-500 text-sm font-bold mb-2"
+                      for="password"
+                    >
+                      Admin Password
+                    </label>
+                    <input
+                      class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                      id="password"
+                      type="password"
+                      placeholder="Password"
+                    />
+                    <label class="md:w-2/3 block text-gray-500 font-bold">
+                      <input class="mr-2 leading-tight" type="checkbox" />
+                      <span class="text-sm">Remember me</span>
+                    </label>
+                  </div>
+                  <div class="flex items-center justify-between">
+                    <button
+                      class="bg-blue-500 hover:bg-blue-700 text-white font-bold w-full py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      type="button"
+                    >
+                      Log In
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default AddReview;
--- src/Pages/HomePage/FAQs/FAQs.js
+++ src/Pages/HomePage/FAQs/FAQs.js
@@ -116,6 +116,77 @@ const FAQs = () => {
           </div>
         </div>
       </div>
+      <div>
+        <div class="w-full">
+          <form class="bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <h1 className="pb-5">ADD A NEW REVIEW</h1>
+            <div class="mb-4">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="username"
+              >
+                WHO GAVE THE REVIEW
+              </label>
+              <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Enter the name"
+              />
+            </div>
+            <div class="mb-6">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="password"
+              >
+                NEW REVIEW
+              </label>
+              <textarea
+                id="message"
+                rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Your message..."
+              ></textarea>
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-state"
+              >
+                REVIEW RATING
+              </label>
+              <div class="relative">
+                <select
+                  class="block appearance-none w-full White border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-state"
+                >
+                  <option>5</option>
+                  <option>4</option>
+                  <option>4.5</option>
+                  <option>3</option>
+                  <option>3.5</option>
+                  <option>2</option>
+                  <option>1</option>
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg
+                    class="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </form>
+          <div className="relative mr-5">
+            <button class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded absolute top-0 right-0">
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
